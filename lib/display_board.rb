ac9362784275ee3ead15bfe0c4eb01f2e88c2640
Araba Taylor <araba.taylor20@gmail.com>
2017-08-25 23:41:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,5 @@ puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
-puts " #{board[5]} | #{board[5]} | #{board[5]} "
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
